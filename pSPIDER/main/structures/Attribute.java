8dc71c89dd7029cb7b567a3a97530e80561cc222
Jakob L. Müller <me@jakob-l-m.de>
2024-02-03 22:52:26 +0100

    added documentation

--- pSPIDER/main/structures/Attribute.java
+++ pSPIDER/main/structures/Attribute.java
@@ -9,8 +9,9 @@ import java.util.Set;
 import lombok.Data;
 
 @Data
-public
-class Attribute {
+
+public class Attribute {
+
 
     private final int id;
     private final String tableName;
@@ -21,39 +22,57 @@ class Attribute {
     private String currentValue;
     private Long currentOccurrences;
 
-    public Attribute(final int id, final String tableName, final String columnName,
-                     final ReadPointer readPointer) {
+    /**
+     * An Attribute resembles a column. It manages its own dependent and referenced attributes.
+     */
+    public Attribute(int id, String tableName, String columnName, ReadPointer readPointer) {
 
         this.id = id;
         this.readPointer = readPointer;
         this.tableName = tableName;
         this.columnName = columnName;
-        dependent = new IntLinkedOpenHashSet();
-        referenced = new IntLinkedOpenHashSet();
+        this.dependent = new IntLinkedOpenHashSet();
+        this.referenced = new IntLinkedOpenHashSet();
         this.currentValue = readPointer.getCurrentValue();
         this.currentOccurrences = Long.parseLong(readPointer.next());
     }
 
+    /**
+     * Adds all dependent ids from the given Set to the internal dependent set.
+     * @param dependent ids of attributes that should be added
+     */
     public void addDependent(final IntSet dependent) {
         this.dependent.addAll(dependent);
     }
 
+    /**
+     * Removes all dependent ids from the given Set from the internal dependent set.
+     * @param dependent ids of attributes that should be removed
+     */
     public void removeDependent(final int dependent) {
         this.dependent.remove(dependent);
     }
 
+    /**
+     * Adds all referenced ids from the given Set to the internal referenced set.
+     * @param referenced ids of attributes that should be added
+     */
     public void addReferenced(final IntSet referenced) {
         this.referenced.addAll(referenced);
     }
 
+    /**
+     * Removes all referenced ids from the given Set from the internal referenced set.
+     * @param referenced ids of attributes that should be removed
+     */
     public void removeReferenced(final int referenced) {
         this.referenced.remove(referenced);
     }
 
-    public String getCurrentValue() {
-        return currentValue;
-    }
-
+    /**
+     * Updates the internal variables currentValue and currentOccurrences
+     * @return True if there was a next value to load, false otherwise
+     */
     public boolean nextValue() {
         if (readPointer.hasNext()) {
             this.currentValue = readPointer.next();
@@ -65,7 +84,13 @@ class Attribute {
         return false;
     }
 
+    /**
+     * Updates the dependant and referenced sets using the attributes that share a value.
+     * @param attributes Set of attribute ids, which share some value
+     * @param attributeIndex The index that stores all attributes
+     */
     public void intersectReferenced(Set<Integer> attributes, final Attribute[] attributeIndex) {
+        // TODO: adjust to respect pINDs
         final IntIterator referencedIterator = referenced.iterator();
         while (referencedIterator.hasNext()) {
             final int ref = referencedIterator.nextInt();
@@ -78,10 +103,19 @@ class Attribute {
         }
     }
 
+    /**
+     * An Attribute is considered as finished, if it is not referenced by any other and is not dependent on any
+     * other attribute. Since it is irrelevant for pIND discovery now, it can be removed from the attribute queue.
+     */
     public boolean isFinished() {
+
         return referenced.isEmpty() && dependent.isEmpty();
     }
 
+    /**
+     * Closes the reader connected to the attribute.
+     * @throws IOException if the reader fails to close
+     */
     public void close() throws IOException {
         readPointer.close();
     }
