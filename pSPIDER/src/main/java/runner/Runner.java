b8cd356ab2d20a242edfc4d4931057bbe41bb76d
Jakob L. Müller <me@jakob-l-m.de>
2024-02-06 20:45:46 +0100

    got rid of unnecessary many config files

--- pSPIDER/src/main/java/runner/Runner.java
+++ pSPIDER/src/main/java/runner/Runner.java
@@ -5,14 +5,13 @@ import de.metanome.algorithm_integration.AlgorithmExecutionException;
 
 import java.io.IOException;
 
-// TODO: Get rid of three config Classes -> everything into one
 // TODO: Null handling options
 // TODO: include some prints for progress tracking
 
 public class Runner {
     public static void main(String[] args) throws AlgorithmExecutionException, IOException {
 
-        Config config = new Config(Config.Algorithm.SPIDER, Config.Dataset.DATA_GOV, 1);
+        Config config = new Config(Config.Dataset.DATA_GOV, 1);
 
         Spider spider = new Spider(config);
         long startTime = System.currentTimeMillis();
