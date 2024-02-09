7057a3a1838275fbedf62d69261adbd3591ac947
Jakob L. Müller <me@jakob-l-m.de>
2024-02-09 09:59:27 +0100

    Documentation and formatting

--- pSPIDER/src/main/java/io/MultiMergeRunner.java
+++ pSPIDER/src/main/java/io/MultiMergeRunner.java
@@ -7,15 +7,26 @@ import structures.MultiwayMergeSort;
 import java.io.IOException;
 import java.util.Queue;
 
+/**
+ * Runner that executes a Multiway MergeSort over a queue of attributes
+ */
 public class MultiMergeRunner extends Thread {
 
     private final Config config;
     private final Queue<Attribute> attributeQueue;
 
+    /**
+     * @param attributeQueue A Queue of attributes which still need to be processed
+     * @param config         The configuration that should be used for the MultiwayMergeSort execution
+     */
     public MultiMergeRunner(Queue<Attribute> attributeQueue, Config config) {
         this.attributeQueue = attributeQueue;
         this.config = config;
     }
+
+    /**
+     * While there are attributes left to be processed, the Thread polls the next in queue and processes it.
+     */
     public void run() {
         MultiwayMergeSort multiwayMergeSort = new MultiwayMergeSort(config);
         while (!attributeQueue.isEmpty()) {
