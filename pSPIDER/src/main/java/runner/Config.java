87431e93a1a9544de5039783ed7d6342a88d8421
Jakob L. Müller <me@jakob-l-m.de>
2024-02-10 11:45:45 +0100

    introduced the variable executionName

--- pSPIDER/src/main/java/runner/Config.java
+++ pSPIDER/src/main/java/runner/Config.java
@@ -23,7 +23,8 @@ public class Config {
     public boolean inputFileHasHeader = true;
     public boolean inputFileSkipDifferingLines = true; // Skip lines that differ from the dataset's schema
     public String inputFileNullString = "";
-    public boolean writeResults = true; // TODO: use this option
+    public boolean writeResults = true;
+    public String executionName = "pSPIDER";
 
     public int numThreads = 6;
 
@@ -41,13 +42,13 @@ public class Config {
     private void setDataset(Config.Dataset dataset) {
         switch (dataset) {
             case KAGGLE -> {
-                this.databaseName = "Kaggle\\";
+                this.databaseName = "Kaggle";
                 this.tableNames = new String[]{"enrollement_schoolmanagement_2", "data", "amazon_laptop_prices_v01", "IQ_level", "Employee", "employee_data (1)"};
                 this.separator = ',';
                 this.inputFileHasHeader = true;
             }
             case TPCH_1 -> {
-                this.databaseName = "TPCH_1\\";
+                this.databaseName = "TPCH_1";
                 this.tableNames = new String[]{"customer", "lineitem", "nation", "orders", "part", "partsupp", "region", "supplier"};
                 this.separator = '|';
                 this.inputFileHasHeader = false;
@@ -55,14 +56,14 @@ public class Config {
                 this.tailingSeparator = true;
             }
             case DATA_GOV -> {
-                this.databaseName = "data.gov\\";
+                this.databaseName = "data.gov";
                 this.tableNames = new String[]{"Air_Quality", "Air_Traffic_Passenger_Statistics", "Crash_Reporting_-_Drivers_Data", "Crime_Data_from_2020_to_Present", "Demographic_Statistics_By_Zip_Code", "diabetes_all_2016", "Electric_Vehicle_Population_Data", "iou_zipcodes_2020", "Lottery_Mega_Millions_Winning_Numbers__Beginning_2002", "Lottery_Powerball_Winning_Numbers__Beginning_2010", "Motor_Vehicle_Collisions_-_Crashes", "National_Obesity_By_State", "NCHS_-_Death_rates_and_life_expectancy_at_birth", "Popular_Baby_Names", "Real_Estate_Sales_2001-2020_GL", "Traffic_Crashes_-_Crashes", "Warehouse_and_Retail_Sales"};
                 this.separator = ',';
                 this.inputFileHasHeader = true;
                 this.fileEnding = ".csv";
             }
             case UEFA -> {
-                this.databaseName = "uefa\\";
+                this.databaseName = "uefa";
                 this.tableNames = new String[]{"attacking", "attempts", "defending", "disciplinary", "distributon", "goalkeeping", "goals", "key_stats"};
                 this.separator = ',';
                 this.inputFileHasHeader = true;
